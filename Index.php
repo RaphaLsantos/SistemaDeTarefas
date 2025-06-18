d60bee544e032f5c6873c5dce8d92b363c86cfba
Raphael da Silva <119632711+RaphaLsantos@users.noreply.github.com>
2025-06-17 22:08:47 -0300

    2 Atualização!
    Adicionei os seguintes Sprints:
    
    - Tela de início (html and css)
    - Tela de Login (html and css)

--- Index.php
+++ Index.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Início</title>
+    <title>100Limites</title>
+    <link rel="stylesheet" href="../SistemaDeTarefas/Css/index.css">
 </head>
+
 <body>
-    
+    <!-- Início nav_bar -->
+    <nav class="nav_bar">
+        <div class="logo">
+            <img src="../SistemaDeTarefas/Img/index/loog.png" alt="Logo 100Limites" class="logo_img">
+        </div>
+        <ul class="info_nav">
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Galeria</a></li>
+            <li><a href="#">Feed</a></li>
+            <li><a href="../SistemaDeTarefas/Login.php">Sou equipe</a></li>
+            <li><a href="#">Local</a></li>
+            <li class="ctt"><a class="ctt_nav" href="#">Contato</a></li>
+        </ul>
+    </nav>
+    <!-- Final nav_bar -->
+
+    <div class="gradiente_fundo"></div>
+    <main class="home">
+        <div class="img_fundo">
+            <img src="../SistemaDeTarefas/Img/index/fundo_home.jpeg" alt="Fundo" class="img_fundo_img">
+        </div>
+
+        <div class="container_home">
+            <div class="info_home">
+                <h1>Onde a arte<br>Encontra a<br>essência.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus.</p>
+                <button class="btn_home" type="submit">Saiba Mais</button>
+            </div>
+
+            <div class="img_fundo_dois">
+                <img src="../SistemaDeTarefas/Img/index/Viva_100_Limites.png" alt="Viva 100 Limites">
+            </div>
+        </div>
+    </main>
+
 </body>
+
 </html>
---
+++ Login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../SistemaDeTarefas/Css/login.css">
+    <title>Login - 100Limites</title>
+</head>
+
+<body>
+    <div class="container">
+        <main class="main_login">
+            <div class="section_1" action="">
+                <form action="">
+                    <h1>Lorem, ipsum dolor.</h1>
+
+                    <input class="text_input" type="text" placeholder="Usuário" required>
+                    <input class="text_input" type="password" placeholder="Senha" required>
+
+                    <div class="links">
+                        <p>
+                            <a class="txt_link" href="#">Esqueci minha senha!</a>
+                        </p>
+                        <p>
+                            <a class="txt_link" href="#">Criar conta!</a>
+                        </p>
+                    </div>
+
+                    <button class="btn_login" type="submit">Entrar</button>
+                </form>
+
+            </div>
+
+            <div class="section_2">
+                <div class="img_section_2">
+                    <a href="../SistemaDeTarefas/Index.php">
+                        <img src="../SistemaDeTarefas/Img/Login/Viva_100_Limites.png" alt="Logo 100Limites" class="logo_img">
+                    </a>
+                </div>
+                <div class="info_section_2">
+                    <h1>Bem-vindo ao 100Limites</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
